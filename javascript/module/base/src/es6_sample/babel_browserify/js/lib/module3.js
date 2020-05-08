d21a11b96a04aff31eeebedced9663c01c6dbd73
Hayatozn8 <43396666+Hayatozn8@users.noreply.github.com>
2020-05-08 17:05:17 +0800

    add

---
+++ javascript/module/base/src/es6_sample/babel_browserify/js/lib/module3.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+// 默认导出
+exports.default = {
+    msg: "default msg",
+    foo: function foo() {
+        console.log("this is module3 default");
+    }
+};
