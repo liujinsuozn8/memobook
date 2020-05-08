df04d9d783e2df3bcec380ff3144b2f5d1b02e68
Hayatozn8 <43396666+Hayatozn8@users.noreply.github.com>
2020-05-08 17:01:20 +0800

    add

---
+++ javascript/module/base/src/es6_sample/babel_browserify/js/src/module1.js
@@ -0,0 +1,10 @@
+// 多次导出
+export function foo(){
+    console.log("this is mdoule1 foo")
+}
+
+export function bar(){
+    console.log("this is mdoule1 bar")
+}
+
+export let arr = [1, 2, "eee", 55]
